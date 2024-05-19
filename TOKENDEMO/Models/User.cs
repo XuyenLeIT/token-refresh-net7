9c56e1dba38d4285e81b53ba004252fd569ad6bc
XuyenLe <letuanxuyen92@gmail.com>
2024-05-19 22:24:20 +0700

    add username required

--- TOKENDEMO/Models/User.cs
+++ TOKENDEMO/Models/User.cs
@@ -10,6 +10,7 @@ namespace TOKENDEMO.Models
         public int Id { get; set; }
         [Required]
         public string Email { get; set; }
+        [Required]
         public string Username { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
