ffd9d6fa530e66932457730c9c9ae01848527952
XuyenLe <letuanxuyen92@gmail.com>
2024-05-19 22:23:07 +0700

    add required

--- TOKENDEMO/Models/User.cs
+++ TOKENDEMO/Models/User.cs
@@ -8,6 +8,7 @@ namespace TOKENDEMO.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
         public string Email { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
